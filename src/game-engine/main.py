14dbbca04f8d059ea8a6503fd044bdec2a57d2e9
Jordy Moos <jordymoos@gmail.com>
2019-12-22 11:37:09 +0100

    Add help to the main command arguments

--- src/game-engine/main.py
+++ src/game-engine/main.py
@@ -39,20 +39,50 @@ The most commonly used commands are:
         parser = argparse.ArgumentParser(
             description='Run a preset')
         parser.add_argument('preset')
-        parser.add_argument('--renderer', choices=['LED', 'PYGAME'], default=defaults.DEFAULT_RENDERER)
-        parser.add_argument('--total-leds', default=defaults.DEFAULT_TOTAL_LEDS)
-        parser.add_argument('--max-leds', default=defaults.DEFAULT_MAX_LEDS_PER_CELL)
-        parser.add_argument('--max-brightness', default=defaults.DEFAULT_MAX_BRIGHTNESS)
-        parser.add_argument('--pair-file', default=defaults.DEFAULT_PAIRING_FILE)
-        parser.add_argument('--view-files', nargs='+', default=defaults.DEFAULT_VIEW_FILES)
-        parser.add_argument('--min-x', default=defaults.DEFAULT_PAIRING_MIN_X)
-        parser.add_argument('--max-x', default=defaults.DEFAULT_PAIRING_MAX_X)
-        parser.add_argument('--min-y', default=defaults.DEFAULT_PAIRING_MIN_Y)
-        parser.add_argument('--max-y', default=defaults.DEFAULT_PAIRING_MAX_Y)
-        parser.add_argument('--grid-width', default=defaults.DEFAULT_GRID_WIDTH)
-        parser.add_argument('--grid-height', default=defaults.DEFAULT_GRID_HEIGHT)
-        parser.add_argument('--serial-port', default=defaults.DEFAULT_SERIAL_PORT)
-        parser.add_argument('--fps', default=defaults.DEFAULT_FPS)
+        parser.add_argument('--renderer',
+                            choices=['LED', 'PYGAME'],
+                            default=defaults.DEFAULT_RENDERER,
+                            help='Use LED to control phisical leds. Use PYGAME for testing. Default: %s' % defaults.DEFAULT_RENDERER)
+        parser.add_argument('--total-leds',
+                            default=defaults.DEFAULT_TOTAL_LEDS,
+                            help='The total number of leds. This must match the configuration on the Teensy. Default: %s' % defaults.DEFAULT_TOTAL_LEDS)
+        parser.add_argument('--max-leds',
+                            default=defaults.DEFAULT_MAX_LEDS_PER_CELL,
+                            help='Max leds per cell (That is an X,Y coordinate in the game), this is handy to reduce bright clusters. Default: %s' % defaults.DEFAULT_MAX_LEDS_PER_CELL)
+        parser.add_argument('--max-brightness',
+                            default=defaults.DEFAULT_MAX_BRIGHTNESS,
+                            help='Possible values between 0 and 255. Scales the brightness of the leds. The leds can be really bright and with this value you can scale that down. Default: %s' % defaults.DEFAULT_MAX_BRIGHTNESS)
+        parser.add_argument('--pair-file',
+                            default=defaults.DEFAULT_PAIRING_FILE,
+                            help='The pair file which holds the led numbers and their positions. This is created by `src/tools/02-process-photos/process.py`. Default: %s' % defaults.DEFAULT_PAIRING_FILE)
+        parser.add_argument('--view-files',
+                            nargs='+',
+                            default=defaults.DEFAULT_VIEW_FILES,
+                            help='One or more view files. One view file is created by `src/tools/02-process-photos/process.py`. Default: %s' % defaults.DEFAULT_VIEW_FILES)
+        parser.add_argument('--min-x',
+                            default=defaults.DEFAULT_PAIRING_MIN_X,
+                            help='This helps cropping the screen. Default: %s' % defaults.DEFAULT_PAIRING_MIN_X)
+        parser.add_argument('--max-x',
+                            default=defaults.DEFAULT_PAIRING_MAX_X,
+                            help='This helps cropping the screen. Default: %s' % defaults.DEFAULT_PAIRING_MAX_X)
+        parser.add_argument('--min-y',
+                            default=defaults.DEFAULT_PAIRING_MIN_Y,
+                            help='This helps cropping the screen. Default: %s' % defaults.DEFAULT_PAIRING_MIN_Y)
+        parser.add_argument('--max-y',
+                            default=defaults.DEFAULT_PAIRING_MAX_Y,
+                            help='This helps cropping the screen. Default: %s' % defaults.DEFAULT_PAIRING_MAX_Y)
+        parser.add_argument('--grid-width',
+                            default=defaults.DEFAULT_GRID_WIDTH,
+                            help='The width of the game screen. Default: %s' % defaults.DEFAULT_GRID_WIDTH)
+        parser.add_argument('--grid-height',
+                            default=defaults.DEFAULT_GRID_HEIGHT,
+                            help='The height of the game screen. Default: %s' % defaults.DEFAULT_GRID_HEIGHT)
+        parser.add_argument('--serial-port',
+                            default=defaults.DEFAULT_SERIAL_PORT,
+                            help='The serial port towards the Teensy. This is only used if `--renderer` is `LED. Default: %s' % defaults.DEFAULT_SERIAL_PORT)
+        parser.add_argument('--fps',
+                            default=defaults.DEFAULT_FPS,
+                            help='How fast the game runs. Default: %s' % defaults.DEFAULT_FPS)
 
         args = parser.parse_args(sys.argv[2:])
         preset = game_presets[args.preset]
